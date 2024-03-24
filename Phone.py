886efae6c49bbabe986ade3feee8e4882f5810e5
Максим Туманов <tumanovmaxim@gmail.com>
2024-03-24 09:46:53 +0300

    Phone class added

--- Phone.py
+++ Phone.py
@@ -1,22 +1,45 @@
-# Родительский класс.
 class Phone:
 
-    # Атрибут базового класса.
     line_type = 'проводной'
 
-    # Инициализатор базового класса.
     def __init__(self, dial_type_value):
         self.dial_type = dial_type_value
 
-    # Метод базового класса.
     def ring(self):
         print('Дзззззыыыыыыыынь!')
 
-    # Ещё один метод базового класса.
     def call(self, phone_number):
         print(f'Звоню по номеру {phone_number}! Тип связи - {self.line_type}.')
 
 
-# Дочерний класс, унаследованный от класса Phone.
 class MobilePhone(Phone):
-    pass 
+    line_type = 'беспроводной'
+    battery_type = 'Li-ion'
+
+    def __init__(self, dial_type_value, network_type):
+        self.network_type = network_type
+        super().__init__(dial_type_value)
+
+    def ring(self):
+        print('Дзынь-дзынь!')
+
+    # Новый метод.
+    def start_game(self):
+        print('Игра запущена!')
+
+
+mobile_phone = MobilePhone('сенсорный', 'LTE')
+
+print(mobile_phone.battery_type)
+print(mobile_phone.network_type)
+mobile_phone.start_game()
+
+...
+
+rotary_phone = Phone('дисковый')
+mobile_phone = MobilePhone('сенсорный', 'LTE')
+
+print(mobile_phone.battery_type)
+print(mobile_phone.network_type)
+mobile_phone.start_game()
+
